3d5a80cf49fd7fb6ff9073b4a727139732bb4e4a
vzahakaylo <volodymyr.zahakailo@currencycloud.com>
2020-07-29 16:25:18 +0300

    Mobile demo

---
+++ mobile.js
@@ -0,0 +1,15 @@
+const {webkit, chromium, firefox, devices} = require('playwright');
+const iPhone11Pro = devices['iPhone 11 Pro'];
+console.log(iPhone11Pro);
+
+(async () => {
+   const browser = await webkit.launch({
+     headless: false,
+   });
+   const context = await browser.newContext({
+    ...iPhone11Pro, 
+   });
+   const page = await context.newPage();
+   await page.goto('https://google.com');
+//   await browser.close();	 
+})();
