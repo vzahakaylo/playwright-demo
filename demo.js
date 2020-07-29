978f57d9dbe692edae38c4b5ca2ddd9de39ac3fa
vzahakaylo <volodymyr.zahakailo@currencycloud.com>
2020-07-29 15:52:36 +0300

    First demo

---
+++ demo.js
@@ -0,0 +1,14 @@
+const {webkit, chromium, firefox} = require('playwright');
+
+(async () => {
+ for (const browserType of [webkit, firefox, chromium]) {
+   const browser = await browserType.launch();
+   const page = await browser.newPage();
+   await page.goto('https://google.com');
+   await page.screenshot({
+     path: `screen-${browserType.name()}.png`,
+   });
+   await browser.close();
+   console.log('success: ' + browserType.name());	
+ } 
+})();
